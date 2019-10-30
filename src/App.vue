6c9fd353e43e9c942d70ca626101647c9dcbe686
ufukyusuf77 <57142930+ufukyusuf77@users.noreply.github.com>
2019-10-30 04:10:15 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <MainHeader></MainHeader>
+  </div>
+</template>
+
+<script>
+  import MainHeader from "./components/MainHeader";
+
+
+export default {
+  name: 'app',
+  components: {
+    MainHeader,
+  }
+}
+
+
+</script>
+
+<style>
+#app {
+  padding: 0 30px 0 30px;
+}
+</style>
